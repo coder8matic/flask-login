c0e09aae4f9db38edaebccc61ab0f850a0062463
coder8matic <coder8matic@gmail.com>
2022-07-05 22:02:17 +0200

    try2

--- models/settings.py
+++ models/settings.py
@@ -1,9 +1,9 @@
 import os
 from sqla_wrapper import SQLAlchemy
 
-db_url = os.getenv("DATABASE_URL")
+db_url = os.getenv("DATABASE_URL").replace("postgres://", "postgresql://", 1)
 print(db_url)
-#                 .replace("postgres://", "postgresql://", 1)
+
 db = SQLAlchemy(db_url)
 
 # db = SQLAlchemy(
