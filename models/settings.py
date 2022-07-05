923c86778689c716e0d1d4abb0df954db030b073
coder8matic <coder8matic@gmail.com>
2022-07-05 21:57:55 +0200

    settings

--- models/settings.py
+++ models/settings.py
@@ -2,6 +2,7 @@ import os
 from sqla_wrapper import SQLAlchemy
 
 db_url = os.getenv("DATABASE_URL")
+print(db_url)
 #                 .replace("postgres://", "postgresql://", 1)
 db = SQLAlchemy(db_url)
 
