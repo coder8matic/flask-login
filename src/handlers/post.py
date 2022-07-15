b6579e9dfcdeb2314b7803b3f92fe79a6b23dc25
coder8matic <coder8matic@gmail.com>
2022-07-15 22:13:07 +0200

    rollback handlers

--- src/handlers/post.py
+++ src/handlers/post.py
@@ -8,38 +8,33 @@ from src.utils.user_helper import (getCurrentUser, isLoggedIn, redirectToLogin,
 post_handlers = Blueprint("post_handlers", __name__)
 
 
-# @post_handlers.route('/post', methods=["GET", "POST"])
-# def createPost():
-#     if request.method == "GET":
-#         return render_template("post_new.html", app_name=app_name) \
-#             if isLoggedIn() else redirectToLogin()
-# elif request.method == "POST":
-#     title = request.form.get('title')
-#     description = request.form.get('description')
-#     author = getCurrentUser()
+@post_handlers.route('/post', methods=["GET", "POST"])
+def createPost():
+    if request.method == "GET":
+        return render_template("post_new.html", app_name=app_name) \
+            if isLoggedIn() else redirectToLogin()
+    elif request.method == "POST":
+        title = request.form.get('title')
+        description = request.form.get('description')
+        author = getCurrentUser()
 
-#     Post.create(title=title, description=description, author=author)
+        Post.create(title=title, description=description, author=author)
 
-#         return redirectToRoute("dashboard.dashboard")
+        return redirectToRoute("dashboard.dashboard")
 
 
 @post_handlers.route('/post/<post_id>', methods=["GET", "POST"])
 def handlePost(post_id):
     if request.method == "GET":
-        if post_id is None:
-            return render_template("post_new.html", app_name=app_name) \
-                if isLoggedIn() else redirectToLogin()
+        print("get method")
+        handlePost = db.query(Post).filter_by(id=post_id).first()
+        if handlePost is None:
+            return redirectToRoute("error.error404")   # redirect to 404
         else:
-            handlePost = db.query(Post).filter_by(id=post_id).first()
-            if handlePost is None:
-                return redirectToRoute("error.error404")   # redirect to 404
-            else:
-                return render_template("post_edit.html",
-                                       app_name=app_name,
-                                       id=handlePost.id,
-                                       title=handlePost.title,
-                                       description=handlePost.description) \
-                    if isLoggedIn() else redirectToLogin()
+            return render_template("post_edit.html", app_name=app_name,
+                                   id=handlePost.id, title=handlePost.title,
+                                   description=handlePost.description) \
+                if isLoggedIn() else redirectToLogin()
 
     elif request.method == "POST":
         if post_id is None:
