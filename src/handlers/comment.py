a04f2e2e81d2ac143582de658c1f6170310f4bb2
coder8matic <coder8matic@gmail.com>
2022-07-21 23:23:07 +0200

    relative routes for comments

--- src/handlers/comment.py
+++ src/handlers/comment.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, make_response, url_for
 from src.models.post import Post
 from src.models.comment import Comment
 from src.models.settings import db
@@ -14,7 +14,8 @@ def postComments(post_id):
     getPost = db.query(Post).filter_by(id=post_id).first()
     if request.method == "GET":
         if getPost is None:
-            return redirectToRoute("error.error404")   # redirect to 404
+            return render_template("404.html", app_name=app_name,
+                           user=getCurrentUser())   # redirect to 404
         getComments = db.query(Comment).filter_by(post_id=post_id) \
                                        .filter_by(deleted_at=None) \
                                        .order_by(Comment.created_at).all()
@@ -46,23 +47,22 @@ def updateComment():
     if request.method == "GET":
         return render_template("404.html", app_name=app_name,
                            user=getCurrentUser())
-    
+
     elif request.method == "POST":
         post_id = request.args.get('post_id', None)
         comment_id = request.args.get('comment_id', None)
-        if request.method == "POST":
-            author_id = getCurrentUser().id
-            comment = request.form.get('updateComment')
-            updateComment = db.query(Comment).filter_by(id=comment_id).first()
-            if author_id == updateComment.author_id:
-                Comment.update(id=comment_id, comment=comment, author_id=author_id)
-                notification_msg = "You have deleted changed comment successfuly!"
-                print(notification_msg)
-            else:
-                notification_msg = "You don't have permissions to change comment!"
-                print(notification_msg)
-            # base_url = url_for(comment_handlers.PostComments(post_id))
-            return redirect(f"/post_comments/{post_id}")  # TODO: change to variable path  # noqa E501
+        author_id = getCurrentUser().id
+        comment = request.form.get('updateComment')
+        updateComment = db.query(Comment).filter_by(id=comment_id).first()
+        if author_id == updateComment.author_id:
+            Comment.update(id=comment_id, comment=comment, author_id=author_id)
+            notification_msg = "You have deleted changed comment successfuly!"
+            print(notification_msg)
+        else:
+            notification_msg = "You don't have permissions to change comment!"
+            print(notification_msg)
+
+    return make_response(redirect(url_for('comment_handlers.postComments', post_id=str(post_id))))
 
 
 @comment_handlers.route('/delete_comments', methods=["POST", "GET"])
@@ -83,6 +83,5 @@ def deleteComment():
         else:
             notification_msg = "You don't have permissions to delete comment!"
             print(notification_msg)
-        # base_url = url_for(comment_handlers.PostComments(post_id))
-        return redirect(f"/post_comments/{post_id}")  # TODO: change to variable path  
-    # noqa E501
+
+        return make_response(redirect(url_for('comment_handlers.postComments', post_id=str(post_id))))
